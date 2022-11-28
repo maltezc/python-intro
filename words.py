14f45dbc6d4397c0a6661c1a81561763c4a8fdeb
Christopher Maltez <cflux.maltez@live.com>
2022-11-28 12:35:07 -0800

    finished last step of exercise

--- words.py
+++ words.py
@@ -2,5 +2,8 @@ def print_upper_words(words, start_letters):
     """Print out each word in upper case on a new line"""
 
     for word in words:
-        if word.upper().startswith(tuple(start_letters)):
-            print(word.upper())
+        for letter in start_letters:
+            if word.upper().startswith(letter.upper()):
+                print(word.upper())
+
+
